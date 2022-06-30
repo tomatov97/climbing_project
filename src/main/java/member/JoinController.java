4c7f6f9fd0fcd2bb8d5509204e544d4b73dd092d
tomatov97 <tngusdl30@gmail.com>
2022-06-30 15:20:34 +0900

    작성한 dao - postman 통해 검증중

--- src/main/java/member/JoinController.java
+++ src/main/java/member/JoinController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import dao.MemberInfoDao;
 import vo.Member;
 
-@WebServlet("/join")
+@WebServlet("/member/join")
 public class JoinController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
